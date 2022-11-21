232c47be10b549d062316fb243c952a707d098af
nguyenngocduck10 <nguyenngocduck10@gmail.com>
2022-11-21 19:34:25 +0700

    second cm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+// import '../public/css/magnific-popup.css'
+// import '../public/css/slicknav.min.css'
+// import '../public/css/font-awesome.min.css'
+// import '../public/css/owl.carousel.min.css'
+// import '../public/css/flaticon.css'
+// import '../public/css/swiper.min.css'
 // import '../public/css/default-css.css'
+// import '../public/css/style.css'
+// import '../public/css/responsive.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
